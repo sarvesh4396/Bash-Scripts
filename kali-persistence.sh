9916dd9e835d2a8c89e02e1cc22abecaea313c58
sarvesh4396 <68162479+sarvesh4396@users.noreply.github.com>
2021-06-11 17:04:29 +0530

    Added kali-persistence scripts

---
+++ kali-persistence.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo "Making persistence...."
+
+# Mounting dir..
+mount_dir = "/mnt/persistence"
+
+# Listing disks
+fdisk -l
+
+# reading partition
+read -p "Enter the partition name [eg: sdb1/sdb2...]: " partition
+        
+conf_dir = "/dev/$partition"
+        
+mkdir -p $mount_dir
+
+mount $conf_dir %mount_dir/
+
+echo "/ union" > $mount_dir/persistence.conf
+
+umount $conf_dir
+
+echo "Setup Complete. Reboot Started."
+
+reboot
+
