8b740a22817e11e3d2940f767d220f59b874cead
sarvesh4396 <68162479+sarvesh4396@users.noreply.github.com>
2021-08-06 02:29:17 +0530

    Added aliases file

---
+++ scripts/alias.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# -*- coding: utf-8 -*-
+
+# This script contains useful aliases
+
+# Remove file securely
+alias rms='shred -uz'
+
+# This will automatically create parent directories if they don’t exist.
+alias mkdir='mkdir -pv'
